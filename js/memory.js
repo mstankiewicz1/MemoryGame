27548004f6c778c8b6f7fcefa27165048e321b91
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2018-09-19 22:33:19 +0200

    Budowa MemoryGame 19.09.18r

--- js/memory.js
+++ js/memory.js
@@ -35,35 +35,69 @@ c11.addEventListener("click", function () { revealCard(11); });
 var oneVisible = false;
 var turnCounter = 0;
 var visible_nr;
+var lock = false;
+var pairsLeft = 6;
 
 
 
 function revealCard(nr) {
-  //  alert(nr);
-    var obraz  = "url(img/" + cards[nr] + ")";
 
-    $('#c' + nr).css('background-image', obraz);
-    $('#c' + nr).addClass('cardA');
-    $('#c' + nr).removeClass('card');
+    var opacityValue = $('#c' + nr).css('opacity');
 
-    if(oneVisible == false){
+    //alert('Opacity: ' + opacityValue);
+
+    if(opacityValue != 0 && lock == false){
+        lock = true;
+        //  alert(nr);
+        var obraz  = "url(img/" + cards[nr] + ")";
+
+        $('#c' + nr).css('background-image', obraz);
+        $('#c' + nr).addClass('cardA');
+        $('#c' + nr).removeClass('card');
+
+        if(oneVisible == false){
             //first card
-        oneVisible = true;
-        visible_nr = nr;
-    }else{
+            oneVisible = true;
+            visible_nr = nr;
+            lock = false;
+        }else{
             //second card
 
-        if(cards[visible_nr] == cards[nr]){
-            alert("para");
-        }else{
-            alert("pudło");
+            if(cards[visible_nr] == cards[nr]){
+                //alert("para");
+                setTimeout(function () {hide2cards(nr, visible_nr)}, 750);
+            }else{
+                //alert("pudło");
+                setTimeout(function () {restore2cards(nr, visible_nr)}, 1000);
+            }
+            turnCounter++;
+            $('.score').html('Turn counter: ' + turnCounter);
+            oneVisible = false;
         }
+    }
+}
 
-        turnCounter++;
-        $('.score').html('Turn counter: ' + turnCounter);
-        oneVisible = false;
+function hide2cards(nr1, nr2) {
+    $('#c' + nr1).css('opacity', '0');
+    $('#c' + nr2).css('opacity', '0');
+
+    pairsLeft--;
+
+    if(pairsLeft == 0){
+        $('.board').html('<h1>You win<br>Done in ' + turnCounter + ' turns</h1>');
     }
 
+    lock = false;
+}
 
+function restore2cards(nr1, nr2) {
+    $('#c' + nr1).css('background-image', 'url(img/karta.png)');
+    $('#c' + nr1).addClass('card');
+    $('#c' + nr1).removeClass('cardA');
 
+    $('#c' + nr2).css('background-image', 'url(img/karta.png)');
+    $('#c' + nr2).addClass('card');
+    $('#c' + nr2).removeClass('cardA');
+
+    lock = false;
 }
